d4e6062fcad826e38c71bb6dd8050a1a40971a94
Thiago Túlio Brandão Silva <47676524+ttbs1@users.noreply.github.com>
2022-03-09 02:42:33 -0300

    navbar created

---
+++ src/components/NavItem.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    props: {
+        text: String,
+        href: String
+    }
+}
+</script>
+
+<template>
+    <li class="nav-item">
+        <a class="nav-link" aria-current="page" href="#">{{text}}</a>
+    </li>
+</template>
+
+<style>
+.nav-link {
+    padding-right: 2em !important;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,80 @@
+<template>
+	<nav class="bg-light pt-3 pb-2">
+		<div class="navbar navbar-light bg-light container">
+			<div class="container-fluid">
+				<a class="navbar-brand">LOGO</a>
+				<form class="d-flex">
+					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+					<button class="btn btn-outline-success" type="submit">Search</button>
+				</form>
+			</div>
+		</div>
+		<div class="bg-sublight">
+			<div class="navbar navbar-expand-lg navbar-light bg-sublight container ps-3">
+				<div class="container-fluid d-flex justify-content-end">
+					<button
+						class="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+							<NavItem text="Home"></NavItem>
+							<li class="nav-item dropdown">
+								<a
+									class="nav-link dropdown-toggle"
+									href="#"
+									id="navbarDropdown"
+									role="button"
+									data-bs-toggle="dropdown"
+									aria-expanded="false"
+								>Dropdown</a>
+								<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+									<li>
+										<a class="dropdown-item" href="#">Action</a>
+									</li>
+									<li>
+										<a class="dropdown-item" href="#">Another action</a>
+									</li>
+									<li>
+										<hr class="dropdown-divider" />
+									</li>
+									<li>
+										<a class="dropdown-item" href="#">Something else here</a>
+									</li>
+								</ul>
+							</li>
+							
+							<NavItem text="About"></NavItem>
+							<NavItem text="FAQs"></NavItem>
+							<NavItem text="Checkout"></NavItem>
+							<NavItem text="Contact"></NavItem>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script>
+import NavItem from "./NavItem.vue";
+
+export default {
+	components: {
+		NavItem
+	}
+}
+</script>
+
+<style>
+.bg-sublight {
+	background-color: #f3f3f3;
+}
+</style>
