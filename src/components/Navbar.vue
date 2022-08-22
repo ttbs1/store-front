2594343086472d8edcc9a8f5d9141ec44c8fdd6b
Thiago Túlio Brandão Silva <47676524+ttbs1@users.noreply.github.com>
2022-08-22 03:16:11 -0300

    some updates

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,8 +4,8 @@
 			<div class="container-fluid">
 				<a class="navbar-brand">LOGO</a>
 				<form class="d-flex">
-					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-					<button class="btn btn-outline-success" type="submit">Search</button>
+					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" readonly/>
+					<button class="btn btn-outline-success disabled" type="submit">Search</button>
 					<font-awesome-icon icon="fa-solid fa-cart-shopping" class="btn btn-lg btn-outline-warning mx-2"/>
 				</form>
 			</div>
